4237cd77133bd66f2a3b34d114f5e28e9789936b
hanxxiiz <labanderohannah@gmail.com>
2025-11-22 19:06:17 +0800

    fixed overflow

--- src/app/admin/reports/page.tsx
+++ src/app/admin/reports/page.tsx
@@ -12,7 +12,7 @@ export default function ReportsPage() {
   const [activeCategory, setActiveCategory] = useState("Feed");
 
   return (
-    <div className="flex flex-col mt-3 space-y-5 w-full px-5">
+    <div className="relative flex flex-col mt-3 space-y-5 w-full px-5">
       <div className="absolute -right-10 w-120 rounded-full bg-[#D0E69080] py-2 px-7 text-2xl font-poppins-bold">
         VIOLATION REPORTS
       </div>
--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 export default function UserManagementPage() {
   return (
-    <div className="flex flex-col mt-3 space-y-5 w-full px-5">
+    <div className="relative flex flex-col mt-3 space-y-5 w-full px-5">
       <div className="absolute -right-10 w-120 rounded-full bg-[#D0E69080] py-2 px-7 text-2xl font-poppins-bold">
         USER MANAGEMENT
       </div>
