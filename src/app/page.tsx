a402fdb1131c46d66cc89ede52beaeefe3cebd9d
Twtzie <zapantatweetie@gmail.com>
2025-11-26 20:44:50 +0800

    dashboard init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Dashboard from "./dashboard/page"; 
-
+import HomePage from "./landing/page";
+import Upload from "./upload/page";
+import { AiChatLoggedIn } from "./AI_chat/AI_chat";
 export default function Home() {
   return (
-    <Dashboard />
+    <>
+      <HomePage/>
+    </>
   );
 }
