1125a5c46bfe751b212941d1674a8d6dfc920d87
Cliff Edward Alsonado <cliffedwardalsonado@gmail.com>
2025-12-06 22:23:00 +0800

    attempt1 in fixing deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Temporarily disable ESLint checks during build for Vercel deployment
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
 }
 
 module.exports = nextConfig
